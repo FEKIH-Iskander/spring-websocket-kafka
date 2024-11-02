3768bea403da365b30484039be41bccc49fc9021
FEKIH-Iskander <102900889+FEKIH-Iskander@users.noreply.github.com>
2024-11-02 08:02:50 +0100

    Project initialization

---
+++ src/main/java/fkh/sk/demo/websocketkafka/Application.java
@@ -0,0 +1,13 @@
+package fkh.sk.demo.websocketkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/fkh/sk/demo/websocketkafka/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fkh.sk.demo.websocketkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
